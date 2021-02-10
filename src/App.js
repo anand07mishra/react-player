773357ad139aa50709ef5910d04a33ce1998a676
pankajkcs@gmail.com <pankajkcs@gmail.com>
2021-02-10 17:15:27 +0530

    google analytics code added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ Amplify.configure(awsconfig);
 onAuthUIStateChange((nextAuthState, authData) => {
   if (nextAuthState === AuthState.SignedIn) {
     console.log("User Email : ", authData.attributes.email);
+    
+    var dataLayer = window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    //gtag('userEmail',new Date()​​);
+    //gtag('config', 'G-X2JYJZ00LL');
+    gtag('config', 'UA-187122323-1');
+
     API.post(apiName, '/postUserInfo', {
       body: {
         "userEmail": authData.attributes.email,
