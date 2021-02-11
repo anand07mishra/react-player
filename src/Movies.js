19d498883c7347d873d5ebe8ca8eb0f1dee0b232
razzrsi <raj.das@rsystems.com>
2021-02-11 12:01:40 +0530

    GA code added in 2 pages

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,17 @@
 import { Typography } from 'antd';
 import React from 'react';
 const { Title } = Typography;
-
+var dataLayer = window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+//gtag('userEmail',new Date()​​);
+//gtag('config', 'G-X2JYJZ00LL');
+gtag('config', 'UA-187122323-1');
 export default function Movies() {
     return (
         <div>
             <Title>Movies</Title>
         </div>
     )
+    
 }
