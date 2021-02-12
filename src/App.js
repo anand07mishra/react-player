af16c8f9c6c20ca5ab0143a55a3d3a1c312cebea
razzrsi <raj.das@rsystems.com>
2021-02-12 13:07:22 +0530

    Enable UserID on app.js page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,20 @@ onAuthUIStateChange((nextAuthState, authData) => {
     console.log("User Email : ", authData.attributes.email);
     
   }
+var imported = document.createElement('script');
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-187122323-2';
+document.head.appendChild(imported);
+var dataLayer=window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-187122323-2');
+gtag('config', 'UA-187122323-2', {
+  'user_id': authData.attributes.email
+});
+  //ga('create','UA-187122323-2','auto');
+  //ga('set','userId',authData.attributes.email);
+  //ga('send','pageview');
+
   if (nextAuthState === AuthState.SignUp) {
     API.post(apiName, '/postUserInfo', {
       body: {
